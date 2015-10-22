0381804331128f973ab7504d2b84a4fda633240e
Lauren Rouse <lauren.l.rouse@gmail.com>
2015-10-21 21:27:26 -0500

    initial fixes

--- javascripts/app.js
+++ javascripts/app.js
@@ -6,7 +6,8 @@ requirejs.config({
         'lodash': '../lib/bower_components/lodash/lodash.min',
         'hbs': '../lib/bower_components/require-handlebars-plugin/hbs',
         'bootstrap': '../lib/bower_components/bootstrap/dist/js/bootstrap.min',
-        'bootstrap-star-rating': '../lib/bower_components/bootstrap-star-rating/js/star-rating.min'
+        'bootstrap-star-rating': '../lib/bower_components/bootstrap-star-rating/js/star-rating.min',
+        'firebase': '../lib/bower_components/firebase/firebase'
     },
     shim: {
         'bootstrap': ['jquery'],
@@ -15,17 +16,15 @@ requirejs.config({
 });
 
 requirejs(
-    ["jquery", "hbs", "bootstrap", "search", "q", 'hbs!../templates/movies', "bootstrap-star-rating"],
-    function($, Handlebars, bootstrap, search, Q, movieTpl, stars) {
-    ["jquery", "hbs", "bootstrap", "search", "q", 'hbs!../templates/movies', 'addfilm'],
-    function($, Handlebars, bootstrap, search, Q, movieTpl, addfilm) {
+    ["jquery", "hbs", "bootstrap", "search", "q", "bootstrap-star-rating", "addfilm", "findid", "search", "signup", "stars", "userAuth"],
+    function($, Handlebars, bootstrap, search, Q, stars, addfilm, findid, search, signup, stars, userAuth)
 
 
       $("#stars").rating({
         min: 0,
         max: 5,
       });
-      
+
 
       // ------- Tab functionality -------
     	$('#myTabs a').click(function (e) {
@@ -34,7 +33,7 @@ requirejs(
 			})
       // ------- End tab functionality -------
 
-  
+
 };
       // ------- search functionality -------
 			$("#search-by-title-button").click(function(e) {
@@ -57,9 +56,9 @@ requirejs(
            	$('#populatee').html(movieTpl(globalJson));
           });
 
-    
+
       // ------- end search functionality -------
     });
 });
 
-  
+
