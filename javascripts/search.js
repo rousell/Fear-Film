211b6b1d61d839735235eb9765b9c8af9a4c41c9
Leif E. Hetland <lehetland1@gmail.com>
2015-10-21 19:19:11 -0500

    attempting last minute styling

--- javascripts/search.js
+++ javascripts/search.js
@@ -7,15 +7,10 @@ define(function(require) {
     var baseUrl= "http://www.omdbapi.com/?s=";
     var movietitle= $("#t").val();
     var URL= baseUrl+movietitle;
-    console.log("variables loaded");
       $.ajax({ url: URL })
         .done(function(json_data) {
-        console.log("Your new movie is", json_data);
-        console.log("search js has finished, resolving json data", json_data)
         $('#populatee').append(json_data);
-        console.log("does it pass?")
         deferred.resolve(json_data);
-        console.log(json_data)
         return json_data
         })
         .fail(function(xhr, status, error) {
