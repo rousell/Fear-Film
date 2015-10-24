4af3321500dbf123109733114c42244c22eb5a37
Allen Phelps <allenatmc@gmail.com>
2015-10-24 12:46:17 -0500

    populate tabs working

--- javascripts/library.js
+++ javascripts/library.js
@@ -1,4 +1,4 @@
-define(["jquery", "firebase", "q", "lodash", "search"], function($, fb, Q, _, search) {
+define(["jquery", "firebase", "q", "lodash", "search", "templates"], function($, fb, Q, _, search, templates) {
 
 	var ref = new Firebase("https://fear-film.firebaseio.com/");
 
@@ -48,7 +48,114 @@ define(["jquery", "firebase", "q", "lodash", "search"], function($, fb, Q, _, se
 
 		populateTabs: function(){
 			console.log('populate tabs');
-		}
+
+			var authData = ref.getAuth();
+      var userUID = authData.uid;
+
+
+      ref.child(userUID +'/library/').on("value", function(snapshot){
+
+      	var userLibrary = snapshot.val();
+      	var userLibraryKeys = _.keys(userLibrary);
+
+      		//templates
+      		var  movieTemplate = templates.basedFilms;
+
+      		//filter for unwatched movies
+      		var unwatchedMovies = {},
+              watchedMovies = {},
+              stars0 = {},
+              stars1 = {},
+              stars2 = {},
+              stars3 = {},
+              stars4 = {},
+              stars5 = {},
+              stars6 = {},
+              stars7 = {},
+              stars8 = {},
+              stars9 = {},
+              stars10 = {};
+
+
+
+      	  userLibraryKeys.forEach(function(key){
+
+            if(userLibrary[key].active){
+
+              var rating = userLibrary[key].userRating;
+              var check;
+
+              if (rating > -1) {
+                check = rating;
+              }
+              //0 stars is essentiall "all movies"
+             stars0[key] = userLibrary[key]
+
+              switch (rating) {
+                case -1:
+                  unwatchedMovies[key] = userLibrary[key]
+                  break;
+                case check:
+                  watchedMovies[key] = userLibrary[key];
+                case 1:
+                  stars1[key] = userLibrary[key]
+                  break;
+                case 2:
+                  stars2[key] = userLibrary[key]
+                  break;
+                case 3:
+                  stars3[key] = userLibrary[key]
+                  break;
+                case 4:
+                  stars4[key] = userLibrary[key]
+                  break;
+                case 5:
+                  stars5[key] = userLibrary[key]
+                  break;
+                case 6:
+                  stars6[key] = userLibrary[key]
+                  break;
+                case 7:
+                  stars7[key] = userLibrary[key]
+                  break;
+                case 8:
+                  stars8[key] = userLibrary[key]
+                  break;
+                case 9:
+                  stars9[key] = userLibrary[key]
+                  break;
+                case 10:
+                  stars10[key] = userLibrary[key]
+                  break;
+                default:
+                  //Statements executed when none of the values match the value of the expression
+                  break;
+              }
+            }
+          });
+
+          console.log(unwatchedMovies);
+          console.log(watchedMovies);
+
+          $("#watchListTab").html(movieTemplate({Search: unwatchedMovies}));
+          $("#viewedTab").html(movieTemplate({Search: watchedMovies}));
+          $("#stars0").html(movieTemplate({Search: stars0}));
+          $("#stars1").html(movieTemplate({Search: stars1}));
+          $("#stars2").html(movieTemplate({Search: stars2}));
+          $("#stars3").html(movieTemplate({Search: stars3}));
+          $("#stars4").html(movieTemplate({Search: stars4}));
+          $("#stars5").html(movieTemplate({Search: stars5}));
+          $("#stars6").html(movieTemplate({Search: stars6}));
+          $("#stars7").html(movieTemplate({Search: stars7}));
+          $("#stars8").html(movieTemplate({Search: stars8}));
+          $("#stars9").html(movieTemplate({Search: stars9}));
+          $("#stars10").html(movieTemplate({Search: stars10}));
+
+
+
+
+     	}); //end firebase snapshot
+		} // end populateTabs
 
   }; //end Return
 });//end Define
--- javascripts/templates.js
+++ javascripts/templates.js
@@ -2,18 +2,16 @@ define(["hbs",
         "hbs!../templates/welcome",
         "hbs!../templates/signUp",
         "hbs!../templates/logIn",
-        "hbs!../templates/movies",
         "hbs!../templates/basedfilms",
         "hbs!../templates/main",
         "hbs!../templates/stars"],
 
-function(handlebars, welcome, signUpTemplate, logInTemplate, moviesTemplate, basedfilmsTemplate, main, starsTemplate) {
+function(handlebars, welcome, signUpTemplate, logInTemplate, basedfilmsTemplate, main, starsTemplate) {
 
   var templates = {};
   templates.welcome = welcome;
   templates.signUp = signUpTemplate;
   templates.logIn = logInTemplate;
-  templates.movies = moviesTemplate;
   templates.basedFilms = basedfilmsTemplate;
   templates.main = main;
   templates.stars = starsTemplate;
